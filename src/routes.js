9820827caee377dba915ade8d1310de65e3f154d
Juan V Lopes <juan.versolato@outlook.com>
2020-02-10 22:16:55 -0400

    Create some routes

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
-import { Router } from 'express'
+import { Router } from 'express';
 
-const routes = new Router()
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
-routes.get('/', (req, res) => {
-    return res.json({ message: 'Hello, World!' })
-})
+import authMiddleware from './app/middlewares/auth';
 
-export default routes
+const routes = new Router();
+
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+
+export default routes;
